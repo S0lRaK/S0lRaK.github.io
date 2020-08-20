ed2b1780fc3f1d0a612b09675c10b288366024e1
S0lRaK <carlosj.gc989@gmail.com>
2020-08-20 11:18:50 -0600

    style: :lipstick: Full page background image

--- src/components/container-full-page.js
+++ src/components/container-full-page.js
@@ -2,5 +2,5 @@ import React from "react"
 import containerFullPageStyles from "./container-full-page.module.css"
 
 export default function ContainerFullPage({ children }) {
-    return <div className={containerFullPageStyles.backgroundImage}>{children}</div>
+    return <div className={containerFullPageStyles.fullPage}>{children}</div>
 }
