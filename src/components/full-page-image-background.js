e9ac51ebd35673b39361021a474aa334f02a7b4e
S0lRaK <carlosj.gc989@gmail.com>
2020-08-20 17:06:24 -0600

    feat(ui): :lipstick: Import and use Permanent Marker font

--- src/components/full-page-image-background.js
+++ src/components/full-page-image-background.js
@@ -1,5 +1,6 @@
 import React from "react"
 import fullPageImageBackgroundStyles from "./full-page-image-background.module.css"
+import "typeface-permanent-marker"
 
 export default function FullPageImageBackground({ children }) {
     return <div className={fullPageImageBackgroundStyles.container}>{children}</div>
