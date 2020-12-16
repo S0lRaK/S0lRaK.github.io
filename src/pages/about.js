a9fa9bc8e8014ba4b83ff5bb2a5951d9c3f3bd75
S0lRaK <carlosj.gc989@gmail.com>
2020-12-15 20:26:46 -0700

    feat: :sparkles: Gatsby tutorial part 3: Create a layout component
    Create layout component to use across pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react"
 import styles from "./about.module.css"
 import Container from "../components/container"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 const User = props => (
   <div className={styles.user}>
@@ -15,23 +16,25 @@ const User = props => (
 
 export default function About() {
   return (
-    <Container>
-      <div style={{ color: `teal` }}>
-        <Header headerText="About Gatsby" />
-        <Header headerText="It's pretty cool" />
-        <p>Such wow. Very React.</p>
-      </div>
-      <User
-        username="Jane Doe"
-        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
-        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicin elit."
-      />
-      <User
-        username="Bob Smith"
-        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
-        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicin elit."
-      />
-    </Container>
+    <Layout>
+      <Container>
+        <div style={{ color: `teal` }}>
+          <Header headerText="About Gatsby" />
+          <Header headerText="It's pretty cool" />
+          <p>Such wow. Very React.</p>
+        </div>
+        <User
+          username="Jane Doe"
+          avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+          excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicin elit."
+        />
+        <User
+          username="Bob Smith"
+          avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+          excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicin elit."
+        />
+      </Container>
+    </Layout>
   )
 }
 
