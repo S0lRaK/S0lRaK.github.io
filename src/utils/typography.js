cd1eede2a3da4218dd2d67f9776f16fc53fbf405
S0lRaK <carlosj.gc989@gmail.com>
2020-12-18 16:28:05 -0700

    docs: :bulb: Comments for Typography library
    Explain the 2 fonts used by the theme selected

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,5 @@
 import Typograpghy from "typography"
+// Theme that sets the font for headers (Work Sans) and body (Quattrocento Sans)
 import fairyGateTheme from "typography-theme-fairy-gates"
 
 const typography = new Typograpghy(fairyGateTheme)
