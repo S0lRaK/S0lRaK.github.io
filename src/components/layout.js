6feef37678acd9d747786c9c7f6ade60fedafeaa
S0lRaK <carlosj.gc989@gmail.com>
2020-12-19 21:49:03 -0700

    refactor: :recycle: Use Emotion styling for Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,49 @@
 import React from "react"
+import { css } from "@emotion/react"
 import { Link } from "gatsby"
 
+import { rhythm } from "../utils/typography"
+
 const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <li
+    css={css`
+      display: inline-block;
+      margin-right: 1rem;
+    `}
+  >
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>MySweetSite</h3>
+          <h3
+            css={css`
+              margin-top: 0;
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+            `}
+          >
+            Pandas Eating Lots
+          </h3>
         </Link>
-        <ul style={{ listStyle: `none`, float: `right` }}>
+        <ul
+          css={css`
+            list-style: none;
+            float: right;
+          `}
+        >
           <ListLink to="/">Home</ListLink>
           <ListLink to="/about/">About</ListLink>
           <ListLink to="/contact/">Contact</ListLink>
