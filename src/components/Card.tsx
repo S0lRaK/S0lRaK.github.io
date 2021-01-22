f2dcf97f6395dd9683aa553dd3259cb9c2e528c8
S0lRaK <carlosj.gc989@gmail.com>
2021-01-21 21:34:38 -0700

    feat(ui): :lipstick: Update card background color to match dark theme.
    Card background color used in projects and posts.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,7 +21,7 @@ export const CardContainer = styled.div<CardContainerProps>`
 `;
 
 export const Card = styled(CardRebass).attrs({
-  bg: 'white',
+  bg: 'gray',
   boxShadow: 0,
 })`
   position: relative;
